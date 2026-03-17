a912429e3fdba99d613a46fb3f0ab4bd055dbba3
Harish Nigam <harishnigam21@gmail.com>
2026-03-17 11:49:29 +0530

    Created Registration Page and added navigation in both registration and login page

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -3,45 +3,32 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import useApi from "@/hooks/useApi";
-import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 import { Data } from "@/types/data";
-import { setLoginStatus, setUser } from "@/redux/slices/UserSlice";
-import { User } from "@/types/user";
-import { AppDispatch } from "@/redux/Store";
 
-export default function Login() {
+export default function Register() {
   const { sendRequest, loading } = useApi();
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<Record<string, string>>({});
   const router = useRouter();
-  const dispatch = useDispatch<AppDispatch>();
-  const handleLogin = async () => {
+  const handleRegistration = async () => {
     //sending request to the backend to verify user
     await sendRequest(
-      "auth/login",
+      "auth/register",
       "POST",
-      { email, password },
+      { name, email, password },
       {},
       false,
     ).then((result) => {
-      const data = result?.data as Data<User> | undefined;
+      const data = result?.data as Data<string> | undefined;
       if (result && result.success) {
         setEmail("");
         setPassword("");
-        toast.success(data?.message || "Successfully Authorized");
-        if (data?.acTk) {
-          window.localStorage.setItem("acTk", JSON.stringify(data.acTk));
-        }
-
-        if (data?.data) {
-          dispatch(setUser(data.data));
-          dispatch(setLoginStatus(true));
-          window.localStorage.setItem("userInfo", JSON.stringify(data.data));
-        }
+        setName("");
         setTimeout(() => {
-          router.push("/dashboard");
+          router.push("/login");
         }, 1000);
       } else {
         if (data?.errors) {
@@ -50,7 +37,7 @@ export default function Login() {
         const errorMessage =
           result?.error || data?.message || "An error occurred";
         toast.error(errorMessage);
-        if (result.status === 403 || result.status === 404) {
+        if (result.status === 409) {
           setTimeout(() => {
             router.push("/register");
           }, 2000);
@@ -63,10 +50,21 @@ export default function Login() {
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-indigo-500 via-purple-500 to-pink-500">
       <div className="w-95 p-8 rounded-2xl backdrop-blur-xl bg-white/10 border border-white/20 shadow-xl">
         <h1 className="text-3xl font-semibold text-white text-center mb-6">
-          Welcome Back
+          Welcome !
         </h1>
 
         <div className="flex flex-col gap-4">
+          <input
+            placeholder="Name"
+            onChange={(e) => {
+              setName(e.target.value);
+              setErrors((prev) => ({ ...prev, name: "" }));
+            }}
+            className="px-4 py-3 rounded-lg bg-white/20 text-white placeholder-white/70 outline-none focus:ring-2 focus:ring-white/40"
+          />
+          {errors["name"] && (
+            <small className="text-red-500">{errors["name"]} !</small>
+          )}
           <input
             placeholder="Email"
             onChange={(e) => {
@@ -91,23 +89,23 @@ export default function Login() {
             <small className="text-red-500">{errors["password"]} !</small>
           )}
           <button
-            onClick={handleLogin}
+            onClick={handleRegistration}
             className="flex items-center justify-center gap-2 mt-2 py-3 rounded-lg bg-white text-indigo-600 font-semibold hover:scale-[1.02] transition cursor-pointer"
           >
             {loading ? (
               <div className="w-5 h-5 border-2 border-indigo-600 border-t-transparent rounded-full animate-spin"></div>
             ) : (
-              "Login"
+              "Register"
             )}
           </button>
         </div>
         <div className="mt-6">
-          Create account ?{" "}
+          Already have account ?{" "}
           <span
             className="text-blue-700 cursor-pointer font-bold"
-            onClick={() => router.push("/register")}
+            onClick={() => router.push("/login")}
           >
-            Register
+            Login
           </span>
         </div>
       </div>
